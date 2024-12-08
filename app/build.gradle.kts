997a742535839f58bc8f5782677619d7d9c5bf43
Bug-pirate <patiladitya914@gmail.com>
2024-12-08 22:21:22 +0530

    UI Modification in HOME, BILLING and ADMIN page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     implementation ("androidx.cardview:cardview:1.0.0")
 
-
+    implementation ("com.google.android.material:material:1.6.1")
     // Glide for image loading
     implementation("com.github.bumptech.glide:glide:4.15.1")
     annotationProcessor("com.github.bumptech.glide:compiler:4.15.1")
